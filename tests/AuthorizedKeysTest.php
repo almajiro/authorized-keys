9c7ba001fb3126c62d48db6323f9929c9f51efb7
Kuroki Almajiro <kuroki@almajiro.net>
2019-11-30 18:11:44 +0900

    Update AuthorizedKeysTest

--- tests/AuthorizedKeysTest.php
+++ tests/AuthorizedKeysTest.php
@@ -5,28 +5,61 @@ namespace Tests;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 use Almajiro\AuthorizedKeys\AuthorizedKeys;
+use Almajiro\AuthorizedKeys\Entities\PublicKey;
 
 class AuthorizedKeysTest extends TestCase
 {
+    private $testContent = <<< __EOF__
+command="ls",no-agent-forwarding ssh-rsa ABCDEFG test@localhost.com
+ssh-dss HIJK test2@globalhost.com
+tunnel="2" ssh-rsa OKOK test@localhost
+no-pty,no-X11-forwarding ssh-rsa hogehoge test@localhost
+no-port-forwarding ssh-rsa hogehogehoge test@hogehoge
+
+__EOF__;
+
     /**
      * @test
      */
     public function readFromFile()
     {
-        $content = <<< __EOF__
-command="ls",no-agent-forwarding ssh-rsa ABCDEFG test@localhost.com
-ssh-dss HIJK test2@globalhost.com
-tunnel="2" ssh-rsa OKOK test@localhost
-no-pty,no-X11-forwarding ssh-rsa test@localhost
-
-__EOF__;
-
         $directory = vfsStream::setup();
         $file = vfsStream::newFile('authorized_keys')
-            ->withContent($content)
+            ->withContent($this->testContent)
             ->at($directory);
 
         $keys = AuthorizedKeys::open($file->url());
-        $this->assertEquals($content, (string)$keys);
+        $this->assertEquals($this->testContent, (string)$keys);
+    }
+
+    public function saveToFile()
+    {
+        $expectedContent = <<<__EOF__
+ssh-rsa ABCDE
+
+__EOF__;
+
+
+        $directory = vfsStream::setup();
+        $file = vfsStream::newFile('authorized_keys')
+            ->at($directory);
+
+        $keys = new AuthorizedKeys();
+        $keys->add(new PublicKey(
+            'ABCDE',
+            'ssh-rsa',
+            [],
+            null
+        ));
+
+        $keys->save($file->url());
+
+        $this->assertTrue($directory->hasChild('authorized_keys'));
+        $this->assertEquals($expectedContent, $file->getContent());
+
+        $this->assertTrue($file->isReadable($file->getUser(), $file->getGroup()), 'File should be readable by the owner');
+        $this->assertTrue($file->isWritable($file->getUser(), $file->getGroup()), 'File should be writable by the owner');
+        $this->assertFalse($file->isReadable('other', 'other'), 'File should not be readable by others');
+        $this->assertFalse($file->isWritable('other', 'other'), 'File should not be writable by others');
     }
 }
