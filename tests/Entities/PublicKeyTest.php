60b558f767fba4aefb8cd23a950170593cd5e794
Kuroki Almajiro <kuroki@almajiro.net>
2019-11-30 14:13:24 +0900

    Add tests

--- tests/Entities/PublicKeyTest.php
+++ tests/Entities/PublicKeyTest.php
@@ -2,6 +2,10 @@
 
 use PHPUnit\Framework\TestCase;
 use Almajiro\AuthorizedKeys\Entities\PublicKey;
+use Almajiro\AuthorizedKeys\Entities\Options\Command;
+use Almajiro\AuthorizedKeys\Entities\Options\NoPty;
+use Almajiro\AuthorizedKeys\Entities\Options\NoAgentForwarding;
+use Almajiro\AuthorizedKeys\Entities\Options\NoPortForwarding;
 
 class PublicKeyTest extends TestCase
 {
@@ -25,5 +29,67 @@ class PublicKeyTest extends TestCase
         $this->assertEquals($expectedType, $publicKey->getType());
         $this->assertEquals([], $publicKey->getOptions());
         $this->assertEquals($expectedComment, $publicKey->getComment());
+
+        return $publicKey;
+    }
+
+    /**
+     * @test
+     */
+    public function setCommandOption()
+    {
+        $publicKey = $this->create();
+        $command = new Command('ls');
+
+        $publicKey->setOptions([
+            $command
+        ]);
+
+        $this->assertEquals($command, $publicKey->getOptions()[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function setNoAgentForwardingOption()
+    {
+        $publicKey = $this->create();
+        $option = new NoAgentForwarding();
+
+        $publicKey->setOptions([
+            $option
+        ]);
+
+        $this->assertEquals($option, $publicKey->getOptions()[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function setNoPortForwardingOption()
+    {
+        $publicKey = $this->create();
+        $option = new NoPortForwarding();
+
+        $publicKey->setOptions([
+            $option
+        ]);
+
+        $this->assertEquals($option, $publicKey->getOptions()[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function setNoPtyOption()
+    {
+        $publicKey = $this->create();
+        $option = new NoPty();
+
+        $publicKey->setOptions([
+            $option
+        ]);
+
+        $this->assertEquals($option, $publicKey->getOptions()[0]);
     }
 }
